35f3096f87a876fea742ea26d6c71022977424e2
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-03-21 15:42:20 +0100

    Updated constructor

--- src/main/java/sk/kosickaakademia/hingis/rat_house/entity/Rat.java
+++ src/main/java/sk/kosickaakademia/hingis/rat_house/entity/Rat.java
@@ -9,12 +9,24 @@ public class Rat {
     private byte age;
     private String color;
     private Gender gender;
+    private static final String HEX_COLOR_PATTERN = "^([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$";
 
     public Rat(String name, byte age, String color, byte gender) {
+
         this.name = name;
+
         this.age = age;
-        this.color = color;
-        this.gender = gender == 0 ? Gender.MALE : gender == 1 ? Gender.FEMALE : null;
+
+        this.color = !color
+                     .matches(HEX_COLOR_PATTERN)
+                     ? "FFFFFF"
+                     : "#" + color;
+
+        this.gender = gender == 0
+                      ? Gender.MALE
+                      : gender == 1
+                      ? Gender.FEMALE
+                      : null;
     }
 
     public Rat(int id, String name, byte age, String color, byte gender) {
