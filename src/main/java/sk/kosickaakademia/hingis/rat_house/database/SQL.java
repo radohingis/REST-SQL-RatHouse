6f0a66ce3bba3572d78409e0f583ff515dc1cc61
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-03-21 17:56:32 +0100

    extended insertRat method conditionals to pass

--- src/main/java/sk/kosickaakademia/hingis/rat_house/database/SQL.java
+++ src/main/java/sk/kosickaakademia/hingis/rat_house/database/SQL.java
@@ -1,5 +1,53 @@
 package sk.kosickaakademia.hingis.rat_house.database;
 
+import sk.kosickaakademia.hingis.rat_house.entity.Rat;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class SQL {
+
+    public boolean insertRat(Rat rat) {
+
+        try(Connection connection = DatabaseConnection.connect()) {
+
+            if (connection == null) {
+                System.out.println("Failed to get connection");
+            }
+
+
+            String insertRatQuery = "insert into rat (name, age, gender, color) values (?,?,?,?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(insertRatQuery);
+
+
+            if(rat.getAge() > 0 || rat.getAge() <= 10
+                                || rat.getGender() < 0
+                                || rat.getGender() > 1
+                                || rat.getName().isEmpty())
+                                return false;
+
+            preparedStatement
+                    .setString(1, rat.getName());
+
+            preparedStatement
+                    .setByte(2, rat.getAge());
+
+            preparedStatement
+                    .setByte(3, rat.getGender());
+
+            preparedStatement
+                    .setString(4, rat.getColor());
+
+            return preparedStatement.execute();
+
+        } catch (SQLException throwables) {
+
+            throwables.printStackTrace();
+        }
+
+        return false;
+    }
 
 }
