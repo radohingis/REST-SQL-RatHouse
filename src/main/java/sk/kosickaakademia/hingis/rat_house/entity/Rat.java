7546c4baf9090afb4fb21c0ba4ce02dd23057b2e
radohingis <radoslav.hingis@kosickaakademia.sk>
2021-03-21 17:45:39 +0100

    deleted enumerator (lacked usability), added insertRat to SQL

--- src/main/java/sk/kosickaakademia/hingis/rat_house/entity/Rat.java
+++ src/main/java/sk/kosickaakademia/hingis/rat_house/entity/Rat.java
@@ -1,14 +1,12 @@
 package sk.kosickaakademia.hingis.rat_house.entity;
 
-import sk.kosickaakademia.hingis.rat_house.enumerator.Gender;
-
 public class Rat {
 
     private int id;
     private String name;
     private byte age;
     private String color;
-    private Gender gender;
+    private byte gender;
     private static final String HEX_COLOR_PATTERN = "^([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$";
 
     public Rat(String name, byte age, String color, byte gender) {
@@ -22,15 +20,31 @@ public class Rat {
                      ? "FFFFFF"
                      : "#" + color;
 
-        this.gender = gender == 0
-                      ? Gender.MALE
-                      : gender == 1
-                      ? Gender.FEMALE
-                      : null;
+        this.gender = gender;
     }
 
     public Rat(int id, String name, byte age, String color, byte gender) {
         this(name, age, color, gender);
         this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public byte getGender() {
+        return gender;
     }
 }
